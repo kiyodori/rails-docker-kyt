f56bdc76fbdd04adc9572f492e301669c3cf6abc
kiyodori <kiyodori@yahoo.co.jp>
2020-12-30 22:29:25 +0900

    CircleCIが動くように、configファイルを設定

--- src/test/controllers/users_controller_test.rb
+++ src/test/controllers/users_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert false
+  end
 end
